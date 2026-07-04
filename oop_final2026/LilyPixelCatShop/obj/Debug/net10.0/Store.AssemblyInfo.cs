f110807e678c26c97f929d343fbe43af22f13cb4
GoofyGoose1 <edridaniel2002@gmail.com>
2026-07-04 23:22:45 +0300

    docker push
    new Docker files for deployment

--- oop_final2026/LilyPixelCatShop/obj/Debug/net10.0/Store.AssemblyInfo.cs
+++ oop_final2026/LilyPixelCatShop/obj/Debug/net10.0/Store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69e10b1ea0f2afa4cdb0db6f9161477c959b685")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8e96269f8f114ef0bd484734c26b59b643ad96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
